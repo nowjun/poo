75e272d61c78b9b54d0e0a84d73374ad8b53e0e2
rtnet-hjpark <hj.park@rtnet.co.kr>
2025-10-24 10:57:02 +0900

    Update run_headless.sh

--- run_headless.sh
+++ run_headless.sh
@@ -3,8 +3,9 @@
 # Build the Docker image
 docker build -t remote-desktop-encoder .
 
-# Run the Docker container in headless mode
+# Run the Docker container with current user permissions
 docker run --gpus all -p 8765:8765 --rm \
+  --user $(id -u):$(id -g) \
   -e DISPLAY=:1 \
   -e QT_X11_NO_MITSHM=1 \
   -e _X11_NO_MITSHM=1 \
